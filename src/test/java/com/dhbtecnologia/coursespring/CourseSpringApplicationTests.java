bc817382aede914e6bb2aa5032085e8115c072f1
romulo lima <dhb.tecnologia@gmail.com>
2021-07-29 19:36:09 -0300

    Project created

---
+++ src/test/java/com/dhbtecnologia/coursespring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhbtecnologia.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
